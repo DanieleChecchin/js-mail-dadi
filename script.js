bdb905f28761b6c61be96f50ef852e05f882096d
DanieleChecchin <danielechecchin26@gmail.com>
2024-08-19 15:53:49 +0200

    email + piccolo form

--- script.js
+++ script.js
@@ -30,3 +30,21 @@ if(playerNumber > pcNumber){
 // Output
 dadi.innerHTML = `Numero del giocatore: ${playerNumber} Numero del computer: ${pcNumber}  ${result}`
 
+
+
+
+
+
+/*
+ESERCIZIO 2 - Finta Login
+Inventa una lista di email autorizzate
+Chiedi all’utente la sua email, con un piccolo form.
+controlla che sia nella lista di chi può accedere,
+stampa un messaggio appropriato sull’esito del controllo.
+Prima di partire a scrivere codice poniamoci qualche domanda:
+Che ci sia un array da qualche parte?
+Cosa devo controllare esattamente?
+*/
+
+// Invento una lista di mail
+const emails = ['daniele@gmail.com', 'giorgio@gmail.com', 'andrea@gmail.com', 'giovanni@gmail.com', 'luca@gmail.com']
